2de3c3f57a525a8ca1a1d2422bef53bced976928
ThijsIllimited <111088301+ThijsIllimited@users.noreply.github.com>
2024-10-10 18:25:55 -0700

    deleted file

--- src/additional_functions.py
+++
@@ -1,7 +0,0 @@
-def replace_brackets(line, temperature_settings):
-    """"Replace the brackets and the brackets contents with the corresponding variable from the dictionary"""
-    for key, value in temperature_settings.items():
-        key_b = '[' + key + ']'
-        if key_b in line:
-            line = line.replace(key_b, str(value))
-    return line
